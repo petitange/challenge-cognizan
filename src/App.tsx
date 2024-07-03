48c5ed82b49888ce14dda767a2114ab548504e56
Angel Pantoja <angelpan17@hotmail.com>
2024-07-03 12:26:54 -0600

    Finished challenge

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react'
+
+interface Character {
+  id: number;
+  name?: string;
+  status: string;
+  species: string;
+  type: string;
+  gender: string;
+  origin: Origin;
+  location: Origin;
+  image?: string;
+  episode: string[];
+  url: string;
+  created: string;
+}
+
+interface Origin {
+  name: string;
+  url: string;
+}
 
 function App() {
+  const [characters, setCharacters] = useState<Character[]>([]);
+
+  const fetchRandomCharacter = async () => {
+    const randomNum = Math.floor(Math.random() * 826) + 1;
+    const response = await fetch(`https://rickandmortyapi.com/api/character/${randomNum}`);
+    const data = await response.json();
+    setCharacters((prevCharacters:any) => [...prevCharacters, data]);
+  };
+
+  useEffect(() => {
+    fetchRandomCharacter(); 
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button className="button-primary" onClick={fetchRandomCharacter}>Add random character</button>
+      <div className="card-container">
+        {characters.map((character:Character, index:number) => (
+          <div key={index} className="card">
+            <img 
+            src={character.image} 
+            alt={character.name} 
+            className="image-container"
+            />
+            <h2>{character.name}</h2>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
